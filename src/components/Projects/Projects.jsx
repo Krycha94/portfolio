8c9a5fde59b4db4d7eb6611d04a4e9402e815e8b
Krystian Kowalski <krycha1515@gmail.com>
2022-12-05 00:21:07 +0100

    Create Projects component with dummy data

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,88 @@
+import styles from "../../styles/components/Projects.module.scss";
+import projectImage from "../../assets/project-1.png";
+
+const dummyProjects = [
+	{
+		id: 1,
+		image: "projectImage",
+		title: "E-commerce",
+		technologies: ["React", "Redux", "Firebase", "Sass"],
+		urlLive: "#",
+		urlCode: "#",
+	},
+	{
+		id: 2,
+		image: "projectImage",
+		title: "E-commerce",
+		technologies: ["React", "Redux", "Firebase", "Sass"],
+		urlLive: "#",
+		urlCode: "#",
+	},
+	{
+		id: 3,
+		image: "projectImage",
+		title: "E-commerce",
+		technologies: ["React", "Redux", "Firebase", "Sass"],
+		urlLive: "#",
+		urlCode: "#",
+	},
+	{
+		id: 4,
+		image: "projectImage",
+		title: "E-commerce",
+		technologies: ["React", "Redux", "Firebase", "Sass"],
+		urlLive: "#",
+		urlCode: "#",
+	},
+	{
+		id: 5,
+		image: "projectImage",
+		title: "E-commerce",
+		technologies: ["React", "Redux", "Firebase", "Sass"],
+		urlLive: "#",
+		urlCode: "#",
+	},
+	{
+		id: 6,
+		image: "projectImage",
+		title: "E-commerce",
+		technologies: ["React", "Redux", "Firebase", "Sass"],
+		urlLive: "#",
+		urlCode: "#",
+	},
+];
+
+const Projects = () => {
+	return (
+		<section className={styles.projects}>
+			<h3>My Projects</h3>
+			<ul className={styles.projects__grid}>
+				{dummyProjects.map((project) => (
+					<li className={styles.projects__card} key={project.id}>
+						{/* image */}
+						<div className={styles.projects__image}>
+							<img src={projectImage} alt={project.title} />
+							<div className={styles.projects__links}>
+								<div className={styles.projects__linksCenter}>
+									<a href="">LIVE</a>
+									<a href="">CODE</a>
+								</div>
+							</div>
+						</div>
+						{/* info */}
+						<div className={styles.projects__info}>
+							<h4>{project.title}</h4>
+							<div className={styles.projects__technologies}>
+								{project.technologies.map((technology, index) => (
+									<span key={index}>{technology}</span>
+								))}
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
+};
+
+export default Projects;
